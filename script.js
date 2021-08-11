cecea8d6efb8ba45a7a87faa7ece356dc02b1ef0
ThiagoMiiranda <thiago.mirandadasilva09@gmail.com>
2021-08-11 17:20:39 -0300

    Add user input for squares per side and better code by using functions

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-const container = document.querySelector('.container');
-
-const tableSize = 512;
-const pixelSize = 16;
-const rowOfPixels = tableSize / pixelSize;
-const totalOfPixels = rowOfPixels * rowOfPixels; //You multiply by itself because the table is a square
-
-for (let i = 0; i < totalOfPixels; i++) {
-    let pixel = document.createElement('div');
-    pixel.style.height = pixelSize + 'px';
-    pixel.style.width = pixelSize + 'px';
-    /*pixel.style.border = 'solid red';*/
-    container.appendChild(pixel);
+function createTable(totalOfPixels) {
+    for (let i = 0; i < totalOfPixels; i++) {
+        let pixel = document.createElement('div');
+        container.appendChild(pixel);
+    }
 }
 
-const pixels = document.querySelectorAll('.container div');
-pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', (e) => {
-        let color = document.querySelector('#colorPicker').value;
-        e.target.style.backgroundColor = color;
+function deleteTable() {
+    let pixels = document.querySelectorAll('.container div');
+    pixels.forEach((pixel) => {
+        container.removeChild(pixel);
     });
-});
+}
+
+function userInput(event) {
+    let output = document.querySelector('output');
+    output.textContent = event.target.value;
+
+    deleteTable();
+
+    let rowOfPixels = event.target.value;
+    let pixelSize = TABLESIZE / rowOfPixels;
+    let totalOfPixels = rowOfPixels * rowOfPixels; //multiply by itself because the table is a square
+
+    container.style.gridTemplateColumns = `repeat(${rowOfPixels}, ${pixelSize}px)`;
+    createTable(totalOfPixels);
+    createPixelEvent();
+}
+
+function createPixelEvent() {
+    let pixels = document.querySelectorAll('.container div');
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseover', (e) => {
+            let color = document.querySelector('#colorPicker').value;
+            e.target.style.backgroundColor = color;
+        });
+    });
+}
+
+const TABLESIZE = 512;
+const container = document.querySelector('.container');
 
 const clearBtn = document.querySelector('#clear-btn');
 clearBtn.addEventListener('click', () => {
+    let pixels = document.querySelectorAll('.container div');
     pixels.forEach((pixel) => {
         pixel.style.backgroundColor = '';
     });
 });
+
+const slider = document.querySelector('#slider');
+slider.addEventListener('input', userInput);
+
+//Basic setup for page load
+container.style.gridTemplateColumns = `repeat(16, 32px)`;
+createTable(16*16);
+createPixelEvent();
